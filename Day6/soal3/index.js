eb95886189999104d1e25d4207c620e195860272
adamrisaldi <32086322+adamrisaldi@users.noreply.github.com>
2023-11-30 09:26:42 +0700

    Update index.js

--- Day6/soal3/index.js
+++ Day6/soal3/index.js
@@ -1,8 +1,6 @@
 let angka1 = 6 
 let angka2 = 5
 
-// jika kondisi yg ada dalam kurung terpenuhi makan akan melakukan kondisi yg di dalam kurung kurawal
-// yang di dalam akan dikonfersi menjadi true atau false
 if (angka2 > angka1) {  
     console.log(true)    
 } else if (angka2 < angka1) {
