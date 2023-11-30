5fdd553d58c8f0345e1a2fa16840b02ffe57310f
adamrisaldi <32086322+adamrisaldi@users.noreply.github.com>
2023-11-30 09:25:20 +0700

    Update index.js

--- Day6/soal1/index.js
+++ Day6/soal1/index.js
@@ -1,35 +1,10 @@
-// let rows1 = '';
-
-// for(let i = 0; i < 5; i++) {
-//     rows1 += '*';
-//     rows1 += '\n';
-// }
-// console.log(rows1);
-
 let rows = 5;
 
 for (rows; rows > 0; rows--) {
     console.log('*');
 }
 
-
 /*------------------------------*/
-
-// let rows2 = '';
-
-// for(let i = 0; i < 5; i++) {
-//     for(let j = 0; j < 5; j++) {
-//         rows2 += '*';
-//     }
-//     rows2 += '\n';
-// }
-// console.log(rows2);
-
-// let rows2 = 5;
-
-// for (rows2; rows2 > 0; rows2--) {
-//     console.log('*****');
-// }
 
 let rows2 = 5;
 
@@ -43,25 +18,6 @@ for (let i = 0; i < rows2; i++) {
 
 /*------------------------------*/
 
-// let rows3 = '';
-
-// for(let i = 0; i < 5; ) {
-//     for(let j = 0; j <= i; j++) {
-//         rows3 += '*';
-//     }
-//     rows3 += '\n';
-//     i++
-// }
-// console.log(rows3);
-
-// let rows3 = 5;
-// let star = ""
-
-// for (let i = 0; i < rows3; i++) {
-//     star += '*'
-//     console.log(star);
-// }
-
 let rows3 = 5;
 let star = '';
 
@@ -73,16 +29,6 @@ for (let i = 0; i < rows3; i++) {
 }
 
 /*------------------------------*/
-
-// let rows4 = '';
-
-// for(let i = 5; i > 0; i--) {
-//     for(let j = 0; j < i; j++) {
-//         rows4 += '*';
-//     }
-//     rows4 += '\n';
-// }
-// console.log(rows4);
 
 let rows4 = 5;
 
